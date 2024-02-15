669cecbf75767438d9dc6913697f23e803f2a956
Alfredo Delgado L <aadelop@gmail.com>
2024-02-15 20:33:40 +0100

    Imported CommonModule, Import modules and Hero interface, plus added hero property to the exported class

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import {  Hero } from "../hero";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
 export class HeroDetailComponent {
-
+  @Input() hero?:Hero;
 }
