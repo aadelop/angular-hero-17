a67bc8c5e645d0b21120d245468dec94a0e1976c
Alfredo Delgado L <aadelop@gmail.com>
2024-02-15 20:34:19 +0100

    removed unused NgFor module

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import {  Hero } from "../hero";
-import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [ CommonModule],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
