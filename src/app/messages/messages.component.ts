d08fbe275a7fdc36b100bcacb0f936e4d37152a9
Alfredo Delgado L <aadelop@gmail.com>
2024-02-16 18:59:27 +0100

    Imported and initialized the MessageService inside the component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
+import {NgFor ,CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [NgFor,CommonModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
 export class MessagesComponent {
-
+  constructor(public messageService: MessageService) {}
 }
