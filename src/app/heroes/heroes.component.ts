d36a0f866bceafe90dac110767a7ac2b028f25be
Alfredo Delgado L <aadelop@gmail.com>
2024-02-16 18:25:53 +0100

    Modified getHeroes function that use HeroService with the Observable rsjx module to get the information in an asynchronous way

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import {NgFor ,CommonModule} from '@angular/common';
 import {  Hero } from "../hero";
 import { FormsModule } from '@angular/forms';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 
@@ -13,10 +13,23 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   styleUrl: './heroes.component.css',
   imports: [CommonModule,FormsModule, NgFor,HeroDetailComponent]
 })
+
+
 export class HeroesComponent {
+  constructor(private heroService: HeroService) {}
+  heroes: Hero [] = [];
   selectedHero?: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  heroes= HEROES;
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  
 }
