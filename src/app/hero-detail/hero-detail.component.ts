40b47b0c47deb0b0840102e32fff08ee8e4a32aa
Alfredo Delgado L <aadelop@gmail.com>
2024-02-16 17:42:38 +0100

    Imported FormsModule component needed to work nG Module properly

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import {  Hero } from "../hero";
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [ CommonModule],
+  imports: [ CommonModule,FormsModule],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css'
 })
