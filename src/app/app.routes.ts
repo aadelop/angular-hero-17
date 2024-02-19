2fd49fc6416b55cc2682a6e911fe8b898954ff78
Alfredo Delgado L <aadelop@gmail.com>
2024-02-19 10:02:11 +0100

    Added new dashboard and empty default routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     {
         path: 'heroes',
         title: 'Heroes list',
         component: HeroesComponent
+    },
+    {
+        path: 'dashboard',
+        title: 'Heroes dashboard',
+        component: DashboardComponent
     }
    
 ];
