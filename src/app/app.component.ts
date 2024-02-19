c2a53085c8dee22cdd798cd964fe2f17acab1faf
Alfredo Delgado L <aadelop@gmail.com>
2024-02-19 10:00:36 +0100

    Imported RouterModule to allow routerLink component work correctly in the principal app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroesComponent  } from "./heroes/heroes.component";
 import { MessagesComponent } from './messages/messages.component';
-import {RouterOutlet} from '@angular/router';
+import {RouterOutlet, RouterModule} from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroesComponent,MessagesComponent,RouterOutlet],
+  imports: [HeroesComponent,MessagesComponent,RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
