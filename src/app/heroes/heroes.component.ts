cd7217564b12a9003bc5de3d543e47bad19fae49
Alfredo Delgado L <aadelop@gmail.com>
2024-02-20 12:59:18 +0100

    Deleted unused onSelect because the isolation of HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { MessageService } from '../message.service';
+import {RouterModule} from '@angular/router';
 
 
 @Component({
@@ -12,24 +13,18 @@ import { MessageService } from '../message.service';
   standalone: true,
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
-  imports: [CommonModule,FormsModule, NgFor,HeroDetailComponent]
+  imports: [CommonModule,FormsModule, NgFor,HeroDetailComponent, RouterModule]
 })
 
 
 export class HeroesComponent {
   
   heroes: Hero [] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService,  private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
