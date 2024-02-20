40186831ae132452b17ebefffa5bafdc98e5b7c8
Alfredo Delgado L <aadelop@gmail.com>
2024-02-20 12:44:54 +0100

    Added the detail route for HeroDetailComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -13,6 +14,11 @@ export const routes: Routes = [
         path: 'dashboard',
         title: 'Heroes dashboard',
         component: DashboardComponent
+    },
+    {
+        path: 'detail/:id',
+        title: 'Hero detail',
+        component: HeroDetailComponent
     }
    
 ];
